477ef24035e3f650bec5bd04fdec99be9654f487
Praveen Napa <np7317143@gmail.com>
2026-02-03 06:17:47 +0530

    your message

--- core/settings.py
+++ core/settings.py
@@ -44,7 +44,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
